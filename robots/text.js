a130fb1f910d5614aa32fe2e24e5a37353feaeff
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-04-02 14:17:04 -0300

    feat: add Watson natural language understanding service

--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,22 @@ const algorithmia = require("algorithmia");
 const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
 const sentencesBoundaryDetection = require("sbd");
 
+const watsonApiKey = require("../credentials/watson-nlu.json").apikey;
+
+const NaturalLanguageUnderstandingV1 = require("watson-developer-cloud/natural-language-understanding/v1.js");
+
+var nlu = new NaturalLanguageUnderstandingV1({
+	iam_apikey: watsonApiKey,
+	version: "2018-04-05",
+	url: "https://gateway.watsonplatform.net/natural-language-understanding/api/"
+});
+
 async function robot(content) {
 	await fetchContentFromWikipedia(content);
 	sanitizeContent(content);
 	breakContentIntoSentences(content);
+	limitMaximumSentences(content);
+	await fetchKeywordsOfAllSentences(content);
 
 	async function fetchContentFromWikipedia(content) {
 		const API_KEY = algorithmiaApiKey;
@@ -13,8 +25,8 @@ async function robot(content) {
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo(
 			"web/WikipediaParser/0.1.2?timeout=300"
 		);
-		const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm);
-		const wikipediaContent = wikipediaResponde.get();
+		const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm);
+		const wikipediaContent = wikipediaResponse.get();
 
 		content.sourceContentOriginal = wikipediaContent.content;
 	}
@@ -58,6 +70,40 @@ async function robot(content) {
 			});
 		});
 	}
+}
+
+async function fetchKeywordsOfAllSentences(content) {
+	for (const sentence of content.sentences) {
+		sentence.keywords = await fetchWatsonAndReturnKeyWords(sentence.text);
+	}
+}
+
+function limitMaximumSentences(content) {
+	content.sentences = content.sentences.slice(0, content.maximumSentences);
+}
+
+async function fetchWatsonAndReturnKeyWords(sentence) {
+	return new Promise((resolve, reject) => {
+		nlu.analyze(
+			{
+				text: sentence,
+				features: {
+					keywords: {}
+				}
+			},
+			(error, response) => {
+				if (error) {
+					throw error;
+				}
+
+				const keywords = response.keywords.map(keyword => {
+					return keyword.text;
+				});
+
+				resolve(keywords);
+			}
+		);
+	});
 }
 
 module.exports = robot;
