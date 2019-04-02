117a1015fa000bc9b95d76c21c1c709db9f8707c
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-04-02 15:14:11 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 const readline = require("readline-sync");
 const trends = require("./trends");
 const robots = {
-	//	userInput : require('./robos/user-inputs.js'),
-	text: require("./robots/text.js")
+	input: require("./robots/input.js"),
+	text: require("./robots/text.js"),
+	state: require("./robots/state.js")
 };
 
 async function start() {
-	const content = {};
+	robots.input();
+	await robots.text();
 
-	// content.searchTerm = askAndReturnSearchTerm();
-	// content.prefix = askAndReturnPrefix();
-	content.searchTerm = "Michael Jackson";
-	content.prefix = 1;
-
-	//	robots.userInput(content);
-	await robots.text(content);
-
-	function askAndReturnSearchTerm() {
-		return readline.question("Type a Wikipedia search term: ");
-	}
-
-	function askAndReturnPrefix() {
-		const prefixes = ["Who is", "What is", "The History of"];
-		const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option");
-		const selectedPrefixText = prefixes[selectedPrefixIndex];
-
-		return selectedPrefixText;
-	}
-
-	console.log(content);
+	const content = robots.state.load();
+	console.dir(content, { depth: null });
 }
 
 start();
--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,28 @@ const algorithmia = require("algorithmia");
 const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
 const sentencesBoundaryDetection = require("sbd");
 
-async function robot(content) {
+const watsonApiKey = require("../credentials/watson-nlu.json").apikey;
+
+const NaturalLanguageUnderstandingV1 = require("watson-developer-cloud/natural-language-understanding/v1.js");
+
+const nlu = new NaturalLanguageUnderstandingV1({
+	iam_apikey: watsonApiKey,
+	version: "2018-04-05",
+	url: "https://gateway.watsonplatform.net/natural-language-understanding/api/"
+});
+
+const state = require("./state.js");
+
+async function robot() {
+	const content = state.load();
+
 	await fetchContentFromWikipedia(content);
 	sanitizeContent(content);
 	breakContentIntoSentences(content);
+	limitMaximumSentences(content);
+	await fetchKeywordsOfAllSentences(content);
+
+	state.save(content);
 
 	async function fetchContentFromWikipedia(content) {
 		const API_KEY = algorithmiaApiKey;
@@ -13,8 +31,8 @@ async function robot(content) {
 		const wikipediaAlgorithm = algorithmiaAuthenticated.algo(
 			"web/WikipediaParser/0.1.2?timeout=300"
 		);
-		const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm);
-		const wikipediaContent = wikipediaResponde.get();
+		const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm);
+		const wikipediaContent = wikipediaResponse.get();
 
 		content.sourceContentOriginal = wikipediaContent.content;
 	}
@@ -58,6 +76,40 @@ async function robot(content) {
 			});
 		});
 	}
+}
+
+async function fetchKeywordsOfAllSentences(content) {
+	for (const sentence of content.sentences) {
+		sentence.keywords = await fetchWatsonAndReturnKeyWords(sentence.text);
+	}
+}
+
+function limitMaximumSentences(content) {
+	content.sentences = content.sentences.slice(0, content.maximumSentences);
+}
+
+async function fetchWatsonAndReturnKeyWords(sentence) {
+	return new Promise((resolve, reject) => {
+		nlu.analyze(
+			{
+				text: sentence,
+				features: {
+					keywords: {}
+				}
+			},
+			(error, response) => {
+				if (error) {
+					throw error;
+				}
+
+				const keywords = response.keywords.map(keyword => {
+					return keyword.text;
+				});
+
+				resolve(keywords);
+			}
+		);
+	});
 }
 
 module.exports = robot;
