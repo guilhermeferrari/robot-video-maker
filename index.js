694dcd016b5c2be90c70cda3187372e7e0f94f34
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-04-03 10:34:13 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const trends = require("./trends");
 const robots = {
 	input: require("./robots/input.js"),
 	text: require("./robots/text.js"),
-	state: require("./robots/state.js")
+	state: require("./robots/state.js"),
+	image: require("./robots/image.js")
 };
 
 async function start() {
-	robots.input();
-	await robots.text();
+	// robots.input();
+	// await robots.text();
+	await robots.image();
 
 	const content = robots.state.load();
 	console.dir(content, { depth: null });
